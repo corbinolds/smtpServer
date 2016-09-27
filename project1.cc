c47742d5326598fe1431f3fb0d7cc3bb8e2dac14
Corbin Olds <Corbin.Olds@nasdaq.com>
2016-09-27 15:00:59 -0600

    Removed not needed function

--- project1.cc
+++ project1.cc
@@ -258,48 +258,42 @@ string sendMail(int sockfd, string recipient, string sender, string message, str
     	return "";
 }
 
-int openSocket() {
-	//handles opening the socket
-	int sockfd, portno, n;
-    	struct sockaddr_in serv_addr;
-    	struct hostent *server;
-
-    	char buffer[256];
- 
-    	portno = 25;
-    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    	if (sockfd < 0) {
-        	cout << "ERROR opening socket" << endl;
-        }
-        string hostname = getTheHost(domain);
-        
-        if(hostname.c_str() == "ERROR"){
-        	return "Error with recipient's host name";
-        }
-        else{
-        	server = gethostbyname(hostname.c_str());
-        }
-    	if (server == NULL) {
-    	    fprintf(stderr,"ERROR, no such host\n");
-    	    exit(0);
-    	}
-    	bzero((char *) &serv_addr, sizeof(serv_addr));
-    	serv_addr.sin_family = AF_INET;
-    	bcopy((char *)server->h_addr, 
-    	     (char *)&serv_addr.sin_addr.s_addr,
-    	     server->h_length);
-    	serv_addr.sin_port = htons(portno);
-    	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-    	    cout << "ERROR connecting" << endl;
-    	bzero(buffer,256);
-
-    	return sockfd;
-}
-
 //command called when RCPT domain is not localhost, opens a socket to the remote host and gives it all the email information. credit for socket handling to http://www.linuxhowtos.org/C_C++/socket.htm
 string connectToSecondarySMTP(string recipient, string sender, string message, string domain) {	
-	int sockfd = openSocket();
-   
+		//handles opening the socket
+	int sockfd, portno, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	char buffer[256];
+
+	portno = 25;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+    	cout << "ERROR opening socket" << endl;
+    }
+    string hostname = getTheHost(domain);
+    
+    if(hostname.c_str() == "ERROR"){
+    	return "Error with recipient's host name";
+    }
+    else{
+    	server = gethostbyname(hostname.c_str());
+    }
+	if (server == NULL) {
+	    fprintf(stderr,"ERROR, no such host\n");
+	    exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, 
+	     (char *)&serv_addr.sin_addr.s_addr,
+	     server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+	    cout << "ERROR connecting" << endl;
+	bzero(buffer,256);
+
     return sendMail(sockfd,recipient, sender, message, domain);
 }
 
